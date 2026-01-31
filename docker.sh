67ba3414931f5b9e14ca3d6c220af06fae52f8c3
sunilsurya0112 <sunilbujji373@gmail.com>
2026-01-31 15:05:45 +0530

    docker-expense

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
+
 growpart /dev/nvme0n1 4
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
@@ -14,3 +15,15 @@ dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+
+#kubernetes installation
+
+#kubectl installation
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+#eksctl installation
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /usr/local/bin
